1e1ab712b4504bb154e2cb8c4a8ded10f8f5295b
liuxi93ing <liuxi93ing@gmail.com>
2017-06-15 22:08:17 -0600

    chapter four added

---
+++ chapter_four.c
@@ -0,0 +1,219 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+// 4. 有三个整数a，b，c，由键盘输入，输出其中最大的数。
+/*
+int main()
+{
+        int a, b, c, t;
+        printf("输入a，b，c的值，以空格分隔：");
+        scanf("%d%d%d",&a,&b,&c);
+        t = a > b? a : b;
+        t = t > c? t : c;
+        printf("最大的数是：%d\n",t);
+        return 0;
+}
+*/
+
+// 5. 从键盘输入一个小于1000的正数，要求输出它的平方根（如果平方根不是整数，则输出其整数部分）。要求输入数据后先对其进行检查是否为小于1000的正数。若不是，则要求重新输入。
+/*
+int main()
+{
+    double num = 0;
+    printf("输入一个小于1000的正数：");
+    scanf("%lf",&num);
+    while(num>=1000||num<=0)
+    {
+        printf("超出范围，请重新输入：");
+        scanf("%lf",&num);
+    }
+
+    printf("其平方根为：%d\n",(int)sqrt(num));
+    return 0；
+}
+*/
+
+// 6. 有一个函数（见课本）， 写程序，输入x的值，输出y对应的值。
+/*
+int main()
+{
+    double x , y;
+    scanf("%lf",&x);
+
+    if(x<1)
+        y = x;
+    else if(x<10)
+        y = 2*x - 1;
+    else
+        y = 3*x - 11;
+
+    printf("%ld\n",y); //%g 输出浮点数时，不会输入多余的0，最多输出6位小数,但是自动四舍五入而导致会丢失精度，不建议使用  (可改成以%g格式输出，分别尝试输入9.9999,和9.99999）
+    return 0;
+}
+*/
+
+// 8. 给出一百分制成绩，要求输出成绩等级‘A’‘B’‘C’‘D’‘E’。90分以上为‘A’，80~89分为‘B’，70~79分为‘C’，60~69分为‘D’，60分一下为‘E’。
+
+/*
+int main()
+{
+    double grade;
+    scanf("%lf",&grade);
+
+    if(grade>100||grade<0)
+        printf("成绩错误");
+    else if(grade>=90)
+        printf("A");    //printf() 不能用单引号来输出单个字符，如printf('A')会报错
+    else if(grade>=80)
+        printf("B");
+    else if(grade>=70)
+        printf("C");
+    else if(grade>=60)
+        printf("D");
+    else
+        printf("E");
+
+    return 0;
+}
+*/
+
+// 9. 给一个不多于5位的正整数，要求：（1） 求出它是几位数 （2）分别输出每一位数字 （3）按逆序输出各位数字，例如原数为321，应输出123.
+
+/*
+int main()
+{
+    int num;
+    printf("输入一个不多于5位的正整数：");
+    scanf("%d",&num);
+    int temp = num;
+
+    int bits = (int)log10((double)num)+1;
+    printf("这是%d位数\n",bits);
+
+
+    printf("每一位数分别为:");
+    while(bits--)
+    {
+        printf(" %d",temp/(int)round(pow(10,bits))); //若直接写成(int)pow(10,bits) 会导致精度丢失造成误差，比如经过浮点运算的期望值为1，但浮点数表示为0.99999，强制转化后(int)0.99999 = 0.
+        temp = temp%(int)round(pow(10,bits));       // pow(),log10() 等数学函数的参数和返回值都应是double类型的，
+    }                                               // 可以把整形当浮点型用（函数参数用整形），但不可以把浮点型直接赋给整形（返回值不能用整形接收），强制转化后也会出现精度丢失，需特别处理
+
+    temp = num;
+    printf("\n逆序每一位数分别为：");
+    while(temp)
+    {
+        printf(" %d",temp%10);
+        temp = temp/10;
+    }
+
+    return 0;
+}
+
+
+*/
+
+
+
+// 10. 企业发放的奖金根据利润提成。利润I低于或等于100000元的，奖金可提10%；利润高于100000元，低于200000元（100000<I<=200000）时，低于100000元的部分按10%提成，
+//     高于100000元的部分，可提成7.5%. 200000<I<=400000时，低于200000元的部分仍按上述办法提成（下同）。高于200000元的部分按5%提成；400000<I<=600000元时，高于400000元的部分按3%提成；
+//     600000<I<=1000000元时，高于600000元的部分按1.5%提成；I>1000000时，超过1000000的部分按1%提成。从键盘输入当月利润I，求应发奖金综述。要求（1）用if语句编程 （2）用switch语句编程
+
+/*
+//if 语句编程
+int main()
+{
+    double I, bouns;
+
+    printf("请输入本月利润：");
+    scanf("%lf",&I);
+
+    if(I <= 100000)
+        bouns = I * 0.1;
+    else if(I <= 200000)
+        bouns = 100000*0.1 + (I-100000)*0.075;
+    else if(I <= 400000)
+        bouns = 100000*0.1 + (200000-100000)*0.075 + (I-200000)*0.05;
+    else if(I <= 600000)
+        bouns = 100000*0.1 + (200000-100000)*0.075 + (400000-200000)*0.05 + (I-400000)*0.03;
+    else if(I <= 1000000)
+        bouns = 100000*0.1 + (200000-100000)*0.075 + (400000-200000)*0.05 + (600000-400000)*0.03 + (I-600000)*0.015;
+    else if(I > 1000000)
+        bouns = 100000*0.1 + (200000-100000)*0.075 + (400000-200000)*0.05 + (600000-400000)*0.03 + (10-600000)*0.015 + (I-1000000)*0.01;
+    printf("应发奖金为：%lf\n",bouns);
+
+    return 0;
+}
+
+
+//switch 语句编程
+
+int main()
+{
+    double I,I2,bouns;
+
+    printf("请输入本月利润：");
+    scanf("%lf",&I);
+
+    I2 = I;
+    int t = (int)I2/100000;
+    switch(t)
+    {
+        case 0: bouns = I* 0.1; break;
+        case 1: bouns = 100000*0.1 + (I-100000)*0.075; break;
+        case 2:
+        case 3: bouns = 100000*0.1 + (200000-100000)*0.075 + (I-200000)*0.05; break;
+        case 4:
+        case 5: bouns = 100000*0.1 + (200000-100000)*0.075 + (400000-200000)*0.05 + (I-400000)*0.03; break;
+        case 6:
+        case 7:
+        case 8:
+        case 9: bouns = 100000*0.1 + (200000-100000)*0.075 + (400000-200000)*0.05 + (600000-400000)*0.03 + (I-600000)*0.015; break;
+        default: bouns = 100000*0.1 + (200000-100000)*0.075 + (400000-200000)*0.05 + (600000-400000)*0.03 + (1000000-600000)*0.015 + (I-1000000)*0.01; break;
+    }
+    printf("应发奖金为：%lf\n",bouns);
+}
+
+*/
+
+// 11. 输入4个整数，要求按由小到大的顺序输出。
+/*
+int main()
+{
+    int max(int a, int b)
+    {
+        return a>b?a:b;
+    }
+
+    int min(int a, int b)
+    {
+        return a<b?a:b;
+    }
+
+    int n1, n2, n3, n4;
+    printf("输入4个整数，以空格分隔：");
+    scanf("%d%d%d%d",&n1,&n2,&n3,&n4);
+
+    int smallest = min(min(n1,n2),min(n3,n4));
+    int largest = max(max(n1,n2),max(n3,n4));
+    int middle_two_sum = n1+n2+n3+n4-smallest-largest;
+
+    if(n1+n2 == middle_two_sum)
+            printf("%d  %d  %d  %d\n",smallest,min(n1,n2),max(n1,n2),largest);
+    else if(n1+n3 == middle_two_sum)
+            printf("%d  %d  %d  %d\n",smallest,min(n1,n3),max(n1,n3),largest);
+    else if(n1+n4 == middle_two_sum)
+            printf("%d  %d  %d  %d\n",smallest,min(n1,n4),max(n1,n4),largest);
+    else if(n2+n3 == middle_two_sum)
+            printf("%d  %d  %d  %d\n",smallest,min(n2,n3),max(n2,n3),largest);
+    else if(n2+n4 == middle_two_sum)
+            printf("%d  %d  %d  %d\n",smallest,min(n2,n4),max(n2,n4),largest);
+    else if(n3+n4 == middle_two_sum)
+            printf("%d  %d  %d  %d\n",smallest,min(n3,n4),max(n3,n4),largest);
+    return 0;
+
+}
+*/
+
+
+// 12. 有4个圆塔，圆心分别为（2，2）、（-2，2）、（-2，-2）、（2，-2），圆半径为1，这4个塔的高度为10米，塔以外无建筑物。今输入任一点的坐标，求该点的建筑高度（塔外的高度为零）。
--- chapter_three.c
+++ chapter_three.c
@@ -1,5 +1,146 @@
-/* 加入我国国民生产总值年增长率为9%，计算10年后我国国民生产总值于现在相比增长了多少百分比。r为增长率，n为年数，p为与现在相比的倍数。
 
+#include<stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+// 1. 假如我国国民生产总值年增长率为9%，计算10年后我国国民生产总值于现在相比增长了多少百分比。r为增长率，n为年数，p为与现在相比的倍数。
+/*
+int main()
+{
+    double r = 0.09;
+    int n = 10;
+    double p = 1;
+
+    int i = 0;
+    for(i = 0; i<n; i++)
+    {
+        p = p*(1 + r);
+    }
+
+    printf("%lf\n",p);
+
+    return 0;
+}
 
 
 */
+
+/** 2. 存款利息的计算。 有1000元，想存5年，可按以下5种办法存：
+  *  （1）一次存5年。
+  *  （2）先存2年期，到期后将本息再存3年。
+  *  （3）先存3年期，到期后将本息再存2年。
+  *  （4）存1年期，到期后将本息再存1年期，连续存五年。
+  *  （5）存活期存款。活期利息每季度结算一次。
+  *  2007年12月银行存款利息如下：
+  *  1年定期存款利息为4.14%；
+  *  2年定期存款利息为4.68%；
+  *  3年定期存款利息为5.4%；
+  *  5年定期存款利息为5.85%；
+  *  活期存款利息为0.72%（活期存款每一季度结算一次利息）。
+  */
+/*
+int main()
+{
+    double interest = 1;
+    int principal = 1000;
+    double r1 = 0.0414;
+    double r2 = 0.0468;
+    double r3 = 0.054;
+    double r5 = 0.0585;
+    double r_s = 0.0072;
+
+    printf("(1)\t%lf\n",principal*(1+r5));
+    printf("(2)\t%lf\n",principal*(1+r2)*(1+r3));
+    printf("(3)\t%lf\n",principal*(1+r3)*(1+r2));
+
+    int i = 0;
+    for(i = 0; i<5; i++)
+        interest = interest*(1 + r1);
+    printf("(4)\t%lf\n",principal*interest);
+
+    i = 0;
+    interest = 1;
+    for(i = 0; i<20; i++)
+        interest = interest*(1 + r_s/4);
+    printf("(5)\t%lf\n",principal*interest);
+
+    return 0;
+}
+*/
+
+
+// 3. 购房从银行贷了一笔款d，主内每月还款额为p，月利率为r，计算多少月能还清。设d为30000元，p为6000元，r为1%、对求得的月份去小数点后一位，对第2位按四舍五入处理。(公式见课本)
+/*
+int main()
+{
+    double d = 30000;
+    double p = 6000;
+    double r = 0.01;
+    double n;
+
+    double numerator = log(p) - log(p - (d * r));
+    double denominator = log(1 + r);
+    n = numerator/denominator;
+
+    n = (int)(n * 10 + 0.5); //利用int向零取正来实现四舍五入
+    n = (double)n/10.0;      //也可以用ceil() floor() 或round() 函数
+    printf("%.1lf\n",n);
+
+    return 0;
+}
+*/
+
+
+// 6. 请编译程序将“China”译成密码，密码规律是：用原来的字母后面第4个字母代替原来的字母。例如，字母“A”后面第4个字母是“E”，用“E”来代替“A”。因此，“China”应该译为“Glmre”。
+//    请编一程序，用赋初值的方式是c1,c2,c3,c4,c5这5个变量的值分别为‘C’，‘h’，‘i’，‘n’，‘a’，经过运算，使c1,c2,c3,c4,c5这5个变量分别变为‘G’‘l’‘m’‘r’‘e’。分别用putchar和printf来输出这5个字符。
+
+/*
+int main()
+{
+    char c1 = 'C';
+    char c2 = 'h';
+    char c3 = 'i';
+    char c4 = 'n';
+    char c1 = 'a';
+
+    c1 = c1 + 4;
+    c2 = c2 + 4;
+    c3 = c3 + 4;
+    c4 = c4 + 4;
+    c5 = c5 + 4;
+
+    putchar(c1);
+    putchar(c2);
+    putchar(c3);
+    putchar(c4);
+    putchar(c5);
+    putchar(\n);
+
+    printf("%c%c%c%c%c\n",c1,c2,c3,c4,c5);
+
+    return 0;
+}
+
+*/
+// 7.设圆的半径r=1.5，圆柱高h=3，求圆周长，圆面积，圆球表面积，圆球体积，圆柱体积。用scanf输入数据，输出计算结果，输出时要求有文字说明，取小数点后2位数字。
+
+/*
+#define PI 3.1415926
+
+int main()
+{
+    double r = 0;
+    double h = 0;
+
+    scanf("请输入圆的半径：%lf",r);
+    scanf("请输入圆柱的高：%lf",h);
+
+    printf("圆的周长是：%0.2lf\n",round(2*PI*r*100)/100);     //round() 是整数位四舍五入的函数
+    printf("圆的面积是：%0.2lf\n",round(PI*r*r*100)/100);
+    printf("圆球的表面积是：%0.2lf\n",round(4*PI*r*r*100)/100);
+    printf("圆柱的体积是：%0.2lf\n",round(3*PI*r*r*r/4*100)/100);
+
+    return 0;
+}
+*/
+
