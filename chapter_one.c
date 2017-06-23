34373c3244896e8f5f8528d8e05282a0c29f9946
liuxi93ing <liuxi93ing@gmail.com>
2017-06-23 13:13:43 -0600

    tes

---
+++ chapter_one.c
@@ -0,0 +1,36 @@
+/*
+// 5.请参照本章例题编写一个程序输出一下信息：
+
+***********************
+Very Good！
+***********************
+
+#include<stdio.h>
+
+int main()
+{
+    printf("********************\n");
+    printf("Very Good!\n");
+    printf("********************\n");
+    return 0;
+}
+
+
+// 6. 编写一个C程序，输入a，b，c 三个值，输出其中最大者。
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c, r;
+    printf("enter a value for a:");
+    scanf("%d",&a);
+    printf("enter a value for b:");
+    scanf("%d",&b);
+    printf("enter a value for c:");
+    scanf("%d",&c);
+    printf("the biggest one is: %d",(a>b?a:b)>c?(a>b?a:b):c );
+}
+
+*/
