08ad2044ac9821d66e58a3a1c78fefabf493d37c
liuxi93ing <liuxi93ing@gmail.com>
2017-10-22 00:18:47 -0600

    chapter nine added

--- chapter_nine.c
+++ chapter_nine.c
@@ -359,8 +359,102 @@ int main()
 
 // 10. 已有a，b两个链表，每个链表中的结点包括学号、成绩。要求把两个链表合并，按学号升序排列。
 
-/** 由题目得知a，b均为静态链表，修改结点中数据比移动结点位置更容易*/
+/** 由题目得知a，b均为静态链表，默认链表排序为通过移动结点位置而不是交换结点中数据。
+    思路为先把两个链表连起来，使用选择排序法，每次选出最小的结点，连成一个新表*/
 
-// 11. 有两个链表a和b，设结点中包含学号、姓名。从a链表中删出于b链表中有相同学号的那些结点。
+#define SIZE sizeof(struct Student)
+struct Student{
+    int number;
+    int score;
+    struct Student *next;
+};
+
+struct Student *concatenate_sort(struct Student *head_1, struct Student *head_2)
+{
+    struct Student *new_head, *cursor, *prev, *min_stu_prev, *min_stu, *ordered=NULL;
+    int min_number;
+
+    cursor = head_1;                                //遍历到第一个链表末尾，接上第二个链表的头
+    while(cursor->next!=NULL)
+        cursor = cursor->next;
+    cursor->next = head_2;
+
+    while(head_1!=NULL)                        // 思路是建立一个新的链表ordered，内层循环每次遍历原链表找出数值最小的
+    {                                          // 结点，从原链表删除该结点，连入ordered链表中
+        min_stu = head_1;                      // 需分情况判断删除结点是否为head，以及新加入结点为ordered中第一个结点的情况
+        min_stu_prev = head_1;
+        min_number = head_1->number;            // 当原表中结点为0时，即head指向NULL，循环结束
+        for(cursor = head_1; cursor!=NULL; cursor = cursor->next)
+        {
+            if(cursor->number<min_number)
+            {
+                min_stu = cursor;               // 为了记录并删除最小数值结点，需记录该结点与该结点前一结点
+                min_stu_prev = prev;
+                min_number = cursor->number;
+            }
+            prev = cursor;
+        }
+
+        if(min_stu==head_1)
+            head_1 = head_1->next;
+        else
+            min_stu_prev->next = min_stu->next;
+
+        if(ordered==NULL)                       // 当第一个结点加入ordered中，order指向它，同时这也是新的头结点（new_head）
+        {
+            ordered = min_stu;
+            new_head = ordered;
+            ordered->next = NULL;
+        }
+        else
+        {
+            ordered->next = min_stu;            // 不是第一个时，连入order->next
+            ordered = ordered->next;
+            ordered->next = NULL;
+        }
+    }
+    return new_head;
+}
+
+void print_s(struct Student *head)
+{
+    struct Student *cursor = head;
+    while(cursor!=NULL)                         //  上一题链表中有收尾结点，因此最后一个结点不输出。本题输出所以结点
+    {                                           //  因此循环结束条件不同
+        printf("学号: %-6d 成绩: %-4d\n", cursor->number, cursor->score);
+        cursor = cursor->next;
+    }
+}
+
+int main()
+{
+    struct Student *head_1, *head_2,*head_3;
+    struct Student a,b,c,d,e,f,g,h;
+
+    head_1 = &a;
+    a.number = 1014; a.score = 97; a.next = &b;
+    b.number = 1007; b.score = 85; b.next = &c;
+    c.number = 1028; c.score = 100; c.next = &d;
+    d.number = 1013; d.score = 79; d.next = NULL;
+    head_2 = &e;
+    e.number = 1011; e.score = 77; e.next = &f;
+    f.number = 1039; f.score = 99; f.next = &g;
+    g.number = 1001; g.score = 93; g.next = &h;
+    h.number = 1022; h.score = 69; h.next = NULL;
+
+    printf("链表1为:\n");
+    print_s(head_1);
+    printf("链表2为:\n");
+    print_s(head_2);
+    head_3 = concatenate_sort(head_1,head_2);
+    printf("排序后的链表为:\n");
+    print_s(head_3);
+    return 0;
+}
+
+
+// 11. 有两个链表a和b，设结点中包含学号、姓名。从a链表中删出与b链表中有相同学号的那些结点。
+/**默认为静态链表，思路是外层循环遍历b表每个结点，内层循环遍历a表每个结点，有相同删除a中结点*/
+
 
 // 12. 建立一个链表，每个结点包含学号、姓名、性别、年龄。输入一个年龄，如果链表中的结点所包含的年龄等于此年龄，则将此结点删去。
